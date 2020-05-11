ba0bed0b919074c7e260776ac4e342de98a1cbf3
Erica Akira <Erica@MacBook-Air-de-Erica.local>
2020-05-11 16:11:22 -0300

    adicionando migrations de usuario, categoria e editora

---
+++ migrations/20200511174024-create_table_usuario.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Comandos de alteração. Cria a tabela
+      Example: 
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+   return queryInterface.createTable(
+      'usuario',
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        nome: {
+          type:Sequelize.STRING(60),
+          allowNull: false
+      },
+        email: {
+          type: Sequelize.STRING(60),
+          allowNull: false
+      },
+        senha: {
+          type: Sequelize.STRING(256),
+          allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Comandos de reversão. Destrói a tabela
+      Example:
+      return queryInterface.dropTable('users');
+    */
+   return queryInterface.dropTable('usuario');
+
+  }
+};
